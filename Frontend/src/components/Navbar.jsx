2c3f635598265e50caa6f707c1a3c7de1598c395
TheGoodCoder12 <satyam.strange93@gmail.com>
2025-01-22 22:57:52 +0530

    Navbar and design of landing page thought of

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
-      
+    <div className='bg-black w-full h-12'>
+      <ul className='flex justify-between p-4'>
+        <div className='transition-all hover:scale-125 cursor-pointer hover:underline duration-500'>logo here</div>
+        <li className='transition-all hover:scale-125 cursor-pointer hover:underline duration-500'>Home</li>
+        <li className='transition-all hover:scale-125 cursor-pointer hover:underline duration-500'>About</li>
+        <li className='transition-all hover:scale-125 cursor-pointer hover:underline duration-500'>Contact</li>
+        <div className='transition-all hover:scale-125 cursor-pointer hover:underline duration-500'>Profile img here</div>
+      </ul>
     </div>
   )
 }
